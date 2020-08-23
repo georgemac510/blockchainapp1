1fd8e79539febc451b1860fa97b2fa60233c15be
georgemac510 <georgemac510@gmail.com>
2020-08-23 17:36:23 -0400

    First commit of upgrade

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './Navbar'
+import Content from './Content'
+import { connect } from 'react-redux'
+import {
+  loadWeb3,
+  loadAccount,
+  loadToken,
+  loadExchange,
+  checkMetamask
+} from '../store/interactions'
+import { contractsLoadedSelector } from '../store/selectors'
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData(this.props.dispatch)
+  }
+
+  async loadBlockchainData(dispatch) {
+    let metamaskDetected = await checkMetamask(window.ethereum, dispatch)
+    if(!metamaskDetected) {
+      window.alert(`Oops! Looks like you don't have Metamask installed. Please install Metamask to access McKurz Capital Token Exchange.`)
+      return
+    }
+    const web3 = await loadWeb3(dispatch)
+    await web3.eth.net.getNetworkType()
+    const networkId = await web3.eth.net.getId()
+    await loadAccount(web3, dispatch)
+    const token = await loadToken(web3, networkId, dispatch)
+    if(!token) {
+      window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+    const exchange = await loadExchange(web3, networkId, dispatch)
+    if(!exchange) {
+      window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    contractsLoaded: contractsLoadedSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ truffle-config.js
@@ -0,0 +1,41 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+// const privateKeys = [process.env.PRIVATE_KEY_ONE, process.env.PRIVATE_KEY_TWO] || ""
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
+  },
+
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+       optimizer: {
+         enabled: false,
+         runs: 200
+      }
+    }
+  }
+}
